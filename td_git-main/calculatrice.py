4be891a281a5b8bc63912099269e4ab4fbf79b70
Lenny Barbe <lenny.barbe@gmail.com>
2021-11-30 14:25:03 +0100

    Ajouté message à celui qui va être dans mon binome

---
+++ td_git-main/calculatrice.py
@@ -0,0 +1,16 @@
+"""
+Ok alors je t'explique maintenant comment on va faire fonctionner ça:
+Il va falloir en premier déterminer chaque étape à réaliser et bien se les répartir.
+Une fois qu'on a bien répartit, on va bosser petit à petit sur le projet chaqun de notre côté.
+Si t'es confus et tu sais pas quoi faire, envoie moi un message via Discord: LennyTheSniper#2929
+
+Sinon, à chaque fois que tu termine de faire ton truc et que tu a besoin que je fasse ma partie
+pour progresser, tu marque juste en bas de ce commentaire dans le deuxième set de guillemet quelle
+est la prochaine étape.
+Voilà voilà, bonne chance!
+"""
+
+
+"""
+
+"""
