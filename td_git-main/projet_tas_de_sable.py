7c976243e2da1e8cdfe60dc4eb524102696485c5
Lenny Barbe <lenny.barbe@gmail.com>
2022-02-07 16:31:10 +0100

    creation quadrillage

--- td_git-main/projet_tas_de_sable.py
+++ td_git-main/projet_tas_de_sable.py
@@ -14,24 +14,39 @@ Mail prof : coline.gianfrotta@ens.uvsq.fr
 
 from random import *
 import tkinter as tk
+from turtle import color
 
 ############### VARIABLES GLOBALES ###############
 
-CANVAS_WIDTH, CANVAS_HEIGHT = 500, 500
+CANVAS_SIZE = 500
 taille_plateau = int(input("Entrez la taille du plateau: "))
+taille_case_SIZE = CANVAS_SIZE // taille_plateau
 
 ################### FONCTIONS ####################
 
+root = tk.Tk()
+root.title("Tas de Sable")
+canvas = tk.Canvas(root, width=CANVAS_SIZE, height=CANVAS_SIZE, bg="black")
 
 def lignes_et_colones():
     global plateau
     plateau = [[0]*taille_plateau]*taille_plateau
 lignes_et_colones()
 
+def quadrillage(nombre_case):
+    x = 0
+    y = 0
+    for i in range(nombre_case):
+        canvas.create_line(x,0,x,y+CANVAS_SIZE, fill="white")
+        canvas.create_line(0,y,x+CANVAS_SIZE,y, fill="white")
+        x += taille_case_SIZE
+        y += taille_case_SIZE
+quadrillage(taille_plateau)
 
-root = tk.Tk()
-root.title("Tas de Sable")
-canvas = tk.Canvas(root, width=CANVAS_WIDTH, height=CANVAS_HEIGHT, bg="black")
+
+
+
+
 
 
 
