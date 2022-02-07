d7296813a5c5833691f7647d133a6bec1dae8f80
Lenny Barbe <lenny.barbe@gmail.com>
2022-02-07 14:48:59 +0100

    ajout du lien vers le dépot

--- td_git-main/projet_tas_de_sable.py
+++ td_git-main/projet_tas_de_sable.py
@@ -2,9 +2,10 @@
 Mail prof : coline.gianfrotta@ens.uvsq.fr
 """
 
-###################
+###########################################
 # MI TD1
 # Zachary MARIANI
 # Lenny BARBE
 # William BRASSART
-###################
+# https://github.com/LennyTheSniper/td_git/
+###########################################
